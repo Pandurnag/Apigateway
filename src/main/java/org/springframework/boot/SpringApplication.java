041db9597a401c95bb2e840d0c41be493f4900d2
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-06-11 13:09:44 +0530

    Apigateway microservices

---
+++ src/main/java/org/springframework/boot/SpringApplication.java
@@ -0,0 +1,27 @@
+package org.springframework.boot;
+
+import com.apigateway.ApigatewayApplication;
+
+public class SpringApplication {
+
+	public static void run11(Class<ApigatewayApplication> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void run111(Class<ApigatewayApplication> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void run1(Class<ApigatewayApplication> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void run(Class<ApigatewayApplication> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
